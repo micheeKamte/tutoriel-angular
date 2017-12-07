89abcdf44852219506b4ad2b4a8a3baead2c2db3
micheeKamte <michee.kamte-kouetche@polymtl.ca>
2017-12-07 15:10:08 -0500

    ajouter le service Hero.service

--- tuto/src/app/heroes/heroes.component.ts
+++ tuto/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +9,20 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
